0c9d2dd57f406dbf1b32f7e44d332c4ecb54942d
harry47900864-lang <harry47900864@gmail.com>
2025-11-23 11:00:46 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,33 @@
+const CACHE_NAME = "self-eval-cache-v1";
+
+const ASSETS = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./manifest.json",
+    "./icons/icon-192.png",
+    "./icons/icon-512.png"
+];
+
+// 安装 SW → 缓存文件
 self.addEventListener("install", (event) => {
     console.log("Service Worker 安装完成");
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
 });
 
+// 激活 SW
 self.addEventListener("activate", (event) => {
     console.log("Service Worker 已激活");
 });
 
-self.addEventListener("fetch", (event) => {});
+// 捕获请求 → 优先从缓存取
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then(res => {
+            return res || fetch(event.request);
+        })
+    );
+});
